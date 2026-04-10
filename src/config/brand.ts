d74875ad2a3db388b118f197facfbe63d0c4af7f
JVSoeiro <joao.vs22@aluno.ifsc.edu.br>
2026-04-10 20:26:50 -0300

    Add cache-busted logo URL to force reloading ufsc_logo.png

--- src/config/brand.ts
+++ src/config/brand.ts
@@ -1,5 +1,5 @@
 export const brandConfig = {
-  partnerLogoSrc: "/ufsc_logo.png",
+  partnerLogoSrc: "/ufsc_logo.png?v=1",
   partnerLogoAlt: "Logo parceiro",
   placeholderLabel: "por [logo enviado]",
 } as const;
